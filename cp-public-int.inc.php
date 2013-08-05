18ad031131191e75ad05187f283b48c0fe5ff1e9
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-05 17:12:26 +0000

    Added tag about the javascript requirement for rendering the calendar.
    git-svn-id: https://plugins.svn.wordpress.org/cp-multi-view-calendar/trunk@751763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cp-public-int.inc.php
+++ cp-public-int.inc.php
@@ -15,7 +15,7 @@ global $wpdb;
 ?>
 <link href="<?php echo plugins_url('/DC_MultiViewCal/css/'.$atts["cssstyle"].'/calendar.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
 <link href="<?php echo plugins_url('/DC_MultiViewCal/css/main.css', __FILE__); ?>" type="text/css" rel="stylesheet" />
-
+<noscript>The CP Multi View Event Calendar requires JavaScript enabled</noscript>
  <div style="z-index:1000;">
     <div id="cal1" class="multicalendar"></div>
  </div>        
